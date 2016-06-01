c769bce17ac346877c0c8ee83ac5df8cfd209c7e
John Jack Franji <jeanjack.franji@gmail.com>
2016-06-01 20:35:50 +0200

    Added Views From Neetha / Pooja

---
+++ src/Controllers/MyCourseController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Threading.Tasks;
+
+namespace Grp.L2PSite.MobileApp.Controllers
+{
+    public class MyCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowSubject(string id)
+        {
+            ViewData["CourseId"] = id;
+            return View();
+        }
+
+
+
+        public IActionResult LearningMaterials()
+        {
+            return View();
+        }
+        public IActionResult SharedDocuments()
+        {
+            return View();
+        }
+
+        public IActionResult Hyperlinks()
+        {
+            return View();
+        }
+
+        public IActionResult Literature()
+        {
+            return View();
+        }
+        public IActionResult MediaLibrary()
+        {
+            return View();
+        }
+
+    }
+}
