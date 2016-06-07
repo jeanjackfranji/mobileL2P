abf7ee8d3421024c0efd2533eb41cbe96bbae14f
John Jack Franji <jeanjack.franji@gmail.com>
2016-06-08 00:43:47 +0200

    Added Amar, Mahboub, Neetha, Pooja and Atul's changes

--- src/Controllers/MyCourseController.cs
+++ src/Controllers/MyCourseController.cs
@@ -4,11 +4,13 @@ namespace Grp.L2PSite.MobileApp.Controllers
 {
     public class MyCourseController : Controller
     {
+        public static string selectedcourse_id;
 
         public IActionResult ShowSubject(string id)
         {
             ViewData["CourseId"] = id;
             AppVariables.cId = id;
+            selectedcourse_id = id;
             return View();
         }
 
@@ -23,11 +25,13 @@ namespace Grp.L2PSite.MobileApp.Controllers
 
         public IActionResult Hyperlinks()
         {
+            ViewData["CourseId"] = selectedcourse_id;
             return View();
         }
 
         public IActionResult Literature()
         {
+            ViewData["CourseId"] = selectedcourse_id;
             return View();
         }
         public IActionResult MediaLibrary()
@@ -37,6 +41,27 @@ namespace Grp.L2PSite.MobileApp.Controllers
 
         public IActionResult Assignments()
         {
+            return View();
+        }
+
+        public IActionResult Announcement()
+        {
+            ViewData["CourseId"] = selectedcourse_id;
+            return View();
+        }
+        public IActionResult AddAnnouncement()
+        {
+            ViewData["CourseId"] = selectedcourse_id;
+            return View();
+        }
+        public IActionResult AddLiterature()
+        {
+            ViewData["CourseId"] = selectedcourse_id;
+            return View();
+        }
+        public IActionResult AddHyperlink()
+        {
+            ViewData["CourseId"] = selectedcourse_id;
             return View();
         }
 
